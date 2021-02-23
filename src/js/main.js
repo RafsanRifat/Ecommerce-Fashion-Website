ee056b2854ba853ca4170b2e24ab39fc7f843598
RafsanRifat <rafsan.rifat255@gmail.com>
2021-02-23 19:05:52 +0600

    scss variables added

---
+++ src/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        navigation:true,
+
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+    //add function
+    
+        $(".red-icon").click(function(){
+            $(this).css("color", "red");
+          });
+        //   $(".red-icon").click(function(){
+        //     $(this).css("color", "#fff");
+        //   });
+
+        //   $(".red-icon").toggle(
+        //     function(){$(".red-icon").css({"color": "red"});},
+        //     function(){$(".red-icon").css({"color": "#fff"});},
+           
+        //   );
+
+
+
+        // COUNTDOWN================
+        // $('#clock').countdown('2021/03/25', function(event) {
+        //     $(this).html(event.strftime('%D days %H:%M:%S'));
+        //   });
+
+  var endDate = "June 7, 2087 15:03:25";
+
+        $('#clock').countdown({
+          date: endDate,
+          render: function(data) {
+            $(this.el).html(
+                "<div class='single-date-wraper'><div class='date-wraper'><h3 id='daysLeft'>" + this.leadingZeros(data.days, 3) + "</h3> </div><p>Days</p></div><div class='single-date-wraper'><div class='date-wraper'><h3 id='daysLeft'>" + this.leadingZeros(data.hours, 2) + " </h3> </div><p>HOURS</p></div><div class='single-date-wraper'><div class='date-wraper'><h3 id='daysLeft'>" + this.leadingZeros(data.min, 2) + " </h3> </div><p>MINS</p></div><div class='single-date-wraper'><div class='date-wraper'><h3 id='daysLeft'>" + this.leadingZeros(data.sec, 2) + " </h3> </div><p>SECONDS</p></div>");
+          }
+        });
+
+
+
+            
+
+  });

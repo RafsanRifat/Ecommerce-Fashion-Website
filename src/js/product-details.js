77c813c8319fd5a17f61950119bf2cc1ddd5c0a9
RafsanRifat <rafsan.rifat255@gmail.com>
2021-03-17 19:10:34 +0600

    product details completed without product num part

--- src/js/product-details.js
+++ src/js/product-details.js
@@ -55,4 +55,22 @@ $('#next-img').click(function (){
     }
   })
 
+
+  
+
+  $('.btn__minus').click(function () {
+    var input = $(this).parent('.input_number').find('.input_value');
+    var currentVal = parseInt(input.val());
+
+    if (currentVal > 0) {
+        input.val(--currentVal);
+    }
+});
+
+$('.btn__plus').click(function () {
+    var input = $(this).parent('.input_number').find('.input_value');
+    var currentVal = parseInt(input.val());
+    input.val(++currentVal);
+});
+
 });

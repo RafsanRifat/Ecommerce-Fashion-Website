91361c4a689c76e03e45717ed2f05e0900ef2e77
RafsanRifat <rafsan.rifat255@gmail.com>
2021-03-20 13:12:39 +0600

    product details page completed with responsive

---
+++ src/js/shopping-cart.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+   
+   
+
+  
+
+  $('.btn__minus').click(function () {
+    var input = $(this).parent('.input_number').find('.input_value');
+    var currentVal = parseInt(input.val());
+
+    if (currentVal > 0) {
+        input.val(--currentVal);
+    }
+});
+
+$('.btn__plus').click(function () {
+    var input = $(this).parent('.input_number').find('.input_value');
+    var currentVal = parseInt(input.val());
+    input.val(++currentVal);
+});
+
+});
